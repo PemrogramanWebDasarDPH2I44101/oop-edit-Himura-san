4c814a7c11cbadeace0eff281d51312dd087cb84
Zahid Musthofainal Akhyar <32158770+Himura-san@users.noreply.github.com>
2018-10-29 17:41:56 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,30 +1,34 @@
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-        <th>Aksi</th>
-    </thead>
-    <tbody>
-<?php
-if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-        $nim = $row['nim'];
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "<td>
-            <a href='form_edit.php?nim=$nim'>Edit</a> | 
-            <a href='delete.php?nim=$nim'>Hapus</a> | 
-            
-            </td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
-</table>
+<div class="container">
+    <div class="row pt-2">
+        <div class="col">
+            <h1 class="text-center pb-2">View Data</h1>
+            <table class="table table-hover table-striped text-center">
+                <thead>
+                    <tr>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Option</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        require("class.php");
+                        $result = $data -> view_data();
+
+                        while($row = $result -> fetch(PDO::FETCH_ASSOC)){
+                        ?>
+                            <tr>
+                                <td><?php echo $row['nim'];?></td>
+                                <td><?php echo $row['nama'];?></td>
+                                <td><?php echo $row['tgl_lahir'];?></td>
+                                <td><a href="index.php?content=edit&nim=<?php echo $row['nim'];?>"><input type="button" class="btn btn-primary" value="Edit"></a> | <a href="class.php?delete=<?php echo $row['nim'];?>" onclick="return confirm('Apakah anda yakin ingin menhapus data ini..?');"><input type="button" class="btn btn-danger" value="Delete"></a></td>
+                            </tr>
+                        <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
